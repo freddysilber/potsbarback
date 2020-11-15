0b6118b475fcb42c69d58973808f4e139467daf4
freddysilber <freddy.silber@gmail.com>
2020-11-15 15:55:26 -0700

    builds login form

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,80 +1,61 @@
 import React from 'react'
-import { Formik, Form, Field, FormikProps, withFormik } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import './Login.scss'
 
-interface FormValues {
+interface InitialValues {
 	email: string,
 	password: string
 }
 
-interface FormProps {
-	email?: string
-	password?: string
-}
-
-const initialValues = {
+const initialValues: InitialValues = {
 	email: '',
 	password: ''
 }
 
-const handleSubmit = (values: any, actions: any) => {
-	console.log({ values, actions })
-	alert(JSON.stringify(values, null, 2))
-	actions.setSubmitting(false)
+const loginValidators = Yup.object().shape({
+	email: Yup.string()
+		.email('Invalid email')
+		.required('Required'),
+	password: Yup.string()
+		.min(2, 'Too Short!')
+		.max(50, 'Too Long!')
+		.required('Required'),
+})
+
+const handleSubmit = (actions: any, values: any) => {
+	console.log('actions', actions)
+	console.log('values', values)
+	alert(JSON.stringify(values, null, 2));
+	actions.setSubmitting(false);
 }
 
-const Login = (props: FormikProps<FormValues>) => {
-	const { touched, errors } = props
-	console.log(props)
-	return (
-		<Formik
-			validateOnChange
-			initialValues={initialValues}
-			validationSchema={
-				Yup.object().shape({
-					email: Yup.string()
-						.required("Email is required")
-						.min(2, "Must enter valid email")
-						.email('Must enter a valid email address'),
-					password: Yup.string()
-						.min(6, "Password must be at least 6 characters")
-						.required("Password is required"),
-				})
-			}
-			onSubmit={(values, actions) => {
-				handleSubmit(values, actions)
-			}}
-		>
-			<Form>
-				<label htmlFor="email">Email</label>
-				<Field className="input login-input is-primary is-medium" id="email" name="email" type="email" placeholder="Enter your email" />
-				{touched.email && errors.email && <div>{errors.email}</div>}
-
-				<button type="submit" disabled={props.isSubmitting}>Submit</button>
-			</Form>
-		</Formik >
-	)
-}
-
-
-const LoginForm = withFormik<FormProps, FormValues>({
-	mapPropsToValues: () => ({
-		email: '',
-		password: ''
-	}),
-	validationSchema: Yup.object().shape({
-		email: Yup.string()
-			.required('Email cannot be empty')
-			.email('Must enter a valid email address')
-	}),
-
-	handleSubmit: (values, { setSubmitting }) => {
-		setTimeout(() => {
-			alert(JSON.stringify(values, null, 2));
-			setSubmitting(false);
-		}, 1000)
-	},
-})(Login);
-
-export default LoginForm;
+const Login = () => (
+	<Formik
+		initialValues={initialValues}
+		validationSchema={loginValidators}
+		onSubmit={(values, actions) => {
+			handleSubmit(actions, values)
+		}}
+	>
+		{({ errors, touched, isSubmitting }) => (
+			<div className="container">
+				<div className="login-title">Bak||Bar Login</div>
+				<div className="card login-input-form">
+					<Form>
+						<label htmlFor="email">Email</label>
+						<Field className="input login-input is-primary is-medium" id="email" name="email" type="email" placeholder="Enter your email" />
+						{touched.email && errors.email && <p className="fieldError">{errors.email}</p>}
+						<label htmlFor="password">Pssword</label>
+						<Field className="input login-input is-primary is-medium" id="password" name="password" type="password" placeholder="Enter your password" />
+						{errors.password && touched.password ? <p className="fieldError">{errors.password}</p> : null}
+						<div className="login-button-div">
+							<button className="button login-button is-primary" type="submit" disabled={isSubmitting} >Submit</button>
+						</div>
+					</Form>
+				</div>
+			</div>
+		)}
+	</Formik>
+)
+export default Login
