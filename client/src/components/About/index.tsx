f93f479c5b8cec064a2c47805e7df04e91c4a510
freddysilber <freddy.silber@gmail.com>
2020-11-20 21:20:19 -0700

    implemets a logout button on the about components

--- client/src/components/About/index.tsx
+++ client/src/components/About/index.tsx
@@ -2,47 +2,71 @@ import React from "react";
 import "./About.scss";
 import axios, { AxiosResponse } from "axios";
 import { Link } from "react-router-dom"
+import { logoutUser } from '../../actions/authActions'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 /**
  * Logo
  *
  * Note: 2:1 ratio
  */
-import bakbar from "../../assets/bakbar.png";
+import bakbar from "../../assets/bakbar.png"
 
-const About: () => JSX.Element = () => {
-  const getUsers: () => void = () => {
+interface AboutProps {
+  logoutUser: any
+}
+
+class About extends React.Component<AboutProps> {
+
+  public static propTypes = {}
+
+  getUsers() {
     axios
       .get("/api/users/getUsers")
       .then((res: AxiosResponse<any>) => console.log(res))
       .catch((err: any) => console.error(err));
   };
 
-  const newUser: () => void = () => {
+  newUser() {
     axios
       .post("/api/users/newUser")
       .then((res: AxiosResponse<any>) => console.log(res))
       .catch((err: any) => console.error(err));
   };
 
-  return (
-    <>
-      <div>WELCOME TO</div>
-      <div>
-        <img src={bakbar} width="400" height="200" alt="logo" />
-      </div>
-      <div className="buttonContainer">
-        <Link to="/Login"><button className="button is-black is-small">
-          Demo.
-        </button></Link>
-        <button className="button is-black is-small" onClick={getUsers}>
-          Get Users.
-        </button>
-        <button className="button is-black is-small" onClick={newUser}>
-          Create user.
-        </button>
-      </div>
-    </>
-  );
-};
+  logoutUser = (event: any) => {
+    // console.log('logout user!', this.props)
+    event.preventDefault()
+    this.props.logoutUser()
+  }
 
-export default About
+  render() {
+    return (
+      <>
+        <div>WELCOME TO</div>
+        <div>
+          <img src={bakbar} width="400" height="200" alt="logo" />
+        </div>
+        <div className="buttonContainer">
+          <Link to="/Login">
+            <button className="button is-black is-small">Demo.</button>
+          </Link>
+          <button className="button is-black is-small" onClick={this.getUsers}>Get Users.</button>
+          <button className="button is-black is-small" onClick={this.newUser}>Create user.</button>
+          <button className="button is-black is-small" onClick={this.logoutUser}>Logout user.</button>
+        </div>
+      </>
+    )
+  }
+}
+
+About.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state: any) => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logoutUser })(About)
