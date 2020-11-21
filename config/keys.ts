f9513eb3fd692cdb54de87c8a7539fe1cf01249a
freddysilber <freddy.silber@gmail.com>
2020-11-21 11:02:13 -0700

    keys.js typings

--- config/keys.ts
+++ config/keys.ts
@@ -1,14 +1,16 @@
+interface MongoAdminUser {
+	username: string,
+	password: string
+}
+
 const DB_NAME: string = 'pots_bak_bar' // Name of the database to use
 // Info for our test user (admin) Mongo requires at least one user in order to connect to the DB
-const TEST_USER: {
-	username: string;
-	password: string;
-} = {
+const TEST_USER: MongoAdminUser = {
 	username: 'Andrew',
 	password: 'Hellosickmoney'
 }
 
-const { username, password } = TEST_USER
+const { username, password }: MongoAdminUser = TEST_USER
 
 module.exports = {
 	mongoURI: `mongodb+srv://${username}:${password}@cluster0.slci9.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
