c384727ca445929c4de4f8da0b760e16f2395ed8
freddysilber <freddy.silber@gmail.com>
2020-11-22 16:20:13 -0700

    portal index refactors

--- client/src/components/Portal/index.tsx
+++ client/src/components/Portal/index.tsx
@@ -1,30 +1,25 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+// Components
 import Staff from "./Staff";
 import Management from "./Management";
 import Checkout from "./Checkout";
 import Verification from "./Verification";
 import Thankyou from "./Thankyou";
+// Styles
 import "./Portal.scss";
+// Routes
 import { Routes } from "../../utils/routes";
 
 const Portal: () => JSX.Element = () => {
 	return (
 		<div className="container">
-			{/* <Switch>
+			<Switch>
 				<Route path={Routes.staff} component={Staff} />
 				<Route path={Routes.management} component={Management} />
 				<Route path={Routes.checkout} component={Checkout} />
 				<Route path={Routes.verification} component={Verification} />
 				<Route path={Routes.thankyou} component={Thankyou} />
-			</Switch> */}
-
-			<Switch>
-				<Route path="/portal/staff" component={Staff} />
-				<Route path="/portal/management" component={Management} />
-				<Route path="/portal/checkout" component={Checkout} />
-				<Route path="/portal/verification" component={Verification} />
-				<Route path="/portal/thankyou" component={Thankyou} />
 			</Switch>
 		</div>
 	);
