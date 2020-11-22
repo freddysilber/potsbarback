97067381a45bf8123ded531c85cdeb80345a590e
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-11-22 13:27:29 -0500

    merge

--- client/src/components/Portal/Admin/index.tsx
+++ client/src/components/Portal/Admin/index.tsx
@@ -2,7 +2,19 @@ import React from "react"
 import './Admin.scss'
 
 const Admin: () => JSX.Element = () => {
-  return <div>This is the Admin Component</div>;
+  return     <div className="management">
+  <div className="columns">
+    <div className="column nav_direct is-one-fifth card">
+    <ul>
+      <li>Today</li>
+      <li>Preformance</li>
+      <li>Schedule</li>
+      <li></li>
+     </ul>   
+    </div>
+    <div className="column content is-two-fifths card">Two </div>
+  </div>
+</div>;
 };
 
 export default Admin;
