970f2ffe18a3f6394017de9537b6dbb42db9e6b0
freddysilber <freddy.silber@gmail.com>
2020-11-27 18:18:57 -0700

    removes closeModal component for now

--- client/src/components/Portal/Checkout/index.tsx
+++ client/src/components/Portal/Checkout/index.tsx
@@ -24,23 +24,24 @@ import { Auth } from '../../Auth'
 // 		return null;
 // 	}
 
-// 	return (
-// 		<div className="modal is-active">
-// 			<div className="modal-background" onClick={closeModal} />
-// 			<div className="modal-card">
-// 				<header className="modal-card-head">
-// 					<p className="modal-card-title">{title}</p>
-// 					<button className="delete" onClick={closeModal} />
-// 				</header>
-// 				<section className="modal-card-body">
-// 					<div className="content">{children}</div>
-// 				</section>
-// 				<footer className="modal-card-foot">
-// 					{/* <CloseModal /> */}
-// 				</footer>
-// 			</div>
+// return (
+// 	<div className="modal is-active">
+// 		<div className="modal-background" onClick={closeModal} />
+// 		<div className="modal-card">
+// 			<header className="modal-card-head">
+// 				<p className="modal-card-title">{title}</p>
+// 				<button className="delete" onClick={closeModal} />
+// 			</header>
+// 			<section className="modal-card-body">
+// 				<div className="content">{children}</div>
+// 			</section>
+// 			<footer className="modal-card-foot">
+// 				<button className="button is-primary" onClick={closeModal}>Cancel</button>
+// 				<button className="button is-primary" onClick={closeModal}>Do something</button>
+// 			</footer>
 // 		</div>
-// 	);
+// 	</div>
+// );
 // };
 
 // Modal.propTypes = {
