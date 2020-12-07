9615aa09908ad28f113dec9b8767f3cb08c25bc7
freddysilber <freddy.silber@gmail.com>
2020-12-06 20:20:40 -0700

    adds providers directory for util

--- client/src/components/Util/index.ts
+++ client/src/components/Util/index.ts
@@ -3,4 +3,4 @@ export { default as Logo } from './components/branding/Logo'
 export { default as LogoWhite } from './components/branding/LogoWhite'
 export { default as Dropdown } from './components/Dropdown'
 // Constant variables
-export * from './constants'
+export * from './providers/constants'
