6d6824a05ce73b9884c5b5616ebd66f0c8b150ab
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2021-01-13 23:22:20 -0500

    test

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
-export { Login, Signup } from './Auth/index'
+export { default as Login} from  './Auth/Login'
+export { default as Signup} from  './Auth/Signup'
 export { default as About } from './About'
 export { default as Portal } from './Portal'
