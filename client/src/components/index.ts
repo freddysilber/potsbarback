55f40c0c29f41eda93254b6044786548017a1229
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2021-01-13 23:26:19 -0500

    kjkj

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Login} from  './Auth/Login'
-export { default as Signup} from  './Auth/Signup'
+export { default as Login} from  './Auth/Login/'
+export { default as Signup} from  './Auth/Signup/'
 export { default as About } from './About'
 export { default as Portal } from './Portal'
