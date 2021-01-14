2b4fcae4c0ba8aa2153d5a2d17ecd33faeb143ae
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2021-01-13 23:28:32 -0500

    test

--- client/src/components/auth/index.ts
+++ client/src/components/auth/index.ts
@@ -1,7 +1,7 @@
 import User from '../../models/User'
 
-export { default as Login } from './Login'
-export { default as Signup } from './Signup'
+export { default as Login } from './Login/index'
+export { default as Signup } from './Signup/index'
 
 export interface Auth {
 	isAuthenticated: boolean,
