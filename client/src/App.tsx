466db16e494f719c47d78a37616ac443f8c533d8
freddysilber <freddy.silber@gmail.com>
2021-02-12 23:17:00 -0700

    scss

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Potsbarbak</p>
+      </div>
+    );
+  }
+}
+
+export default App;
