5200bf2e9f24a1f1f2635f537f7daebe8f211d7c
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2021-02-18 18:31:07 -0500

    Hello

--- client/src/components/Portal/Navbar/index.tsx
+++ client/src/components/Portal/Navbar/index.tsx
@@ -4,25 +4,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <div className="title ml-4"> nav</div>
-        </a>
-
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
 
       <div id="navbarBasicExample" className="navbar-menu">
+      <a
+        role="button"
+        className="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
         <div className="navbar-start">
           <Link to="/portal/staff" className="navbar-item">
             Shift
@@ -30,6 +23,7 @@ const Navbar = () => {
           <Link to="/portal/events" className="navbar-item">
             Events
           </Link>
+
           <Link to="/portal/schedule" className="navbar-item">
             Schedule
           </Link>
@@ -43,17 +37,26 @@ const Navbar = () => {
         </div>
 
         <div className="navbar-end">
-          <div className="navbar-item">
+          <div className="navbar-item ">
             <div className="navbar-item has-dropdown is-hoverable">
               <a className="navbar-link">Admin</a>
 
               <div className="navbar-dropdown">
-                <Link to="/portal/management" className="navbar-item">Management</Link>
-                <Link to="/portal/comptrolling" className="navbar-item">Comptrolling</Link>
-                <Link to="/portal/admin/scheduleadmin" className="navbar-item">Schedules Admin</Link>
-                <Link to="/portal/admin/eventsadmin" className="navbar-item">Events Admin</Link>
-                <Link to="/portal/admin/employeeadmin" className="navbar-item">Employee Admin</Link>
-
+                <Link to="/portal/management" className="navbar-item">
+                  Management
+                </Link>
+                <Link to="/portal/comptrolling" className="navbar-item">
+                  Comptrolling
+                </Link>
+                <Link to="/portal/admin/scheduleadmin" className="navbar-item">
+                  Schedules Admin
+                </Link>
+                <Link to="/portal/admin/eventsadmin" className="navbar-item">
+                  Events Admin
+                </Link>
+                <Link to="/portal/admin/employeeadmin" className="navbar-item">
+                  Employee Admin
+                </Link>
 
                 <a className="navbar-item">Report an issue</a>
               </div>
