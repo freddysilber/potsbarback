93ec500f28a39c98cffc854f63f634a6019b7c63
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2021-03-14 23:23:24 -0400

    added secret

--- client/src/components/Portal/Events/index.tsx
+++ client/src/components/Portal/Events/index.tsx
@@ -30,39 +30,6 @@ const dummyData = [
 ];
 
 const Events = () => {
-  const [value, onChange] = useState<Date | Date[]>(new Date());
-  const [date, _date] = useState(Date());
-  return (
-
-      <div className="card px-4 py-4">
-        <div className="title">Events</div>
-        <Calendar onChange={onChange} value={value} />
-		{/* <Bar /> */}
-      </div>
-        
-
-  );
-	{
-		id: 0,
-		first_name: "Andrew",
-		last_name: "Pedersen",
-		phone: "9705313993",
-		is_active: true,
-		job_role: 1,
-		active_shift: true,
-		check_out: {
-			cash_sales: 476.98,
-			credit_sales: 9889.0,
-			credit_tips: 976.89,
-			is_closed: false,
-			is_verified: false,
-			is_recieved: false,
-			cost_center: 0,
-			starting_cash: 250.0,
-		},
-	}]
-
-const Events = () => {
 	const [value, onChange] = useState<Date | Date[]>(new Date());
 	const [date, _date] = useState(Date())
 	return (
